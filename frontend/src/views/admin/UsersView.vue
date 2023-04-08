02b4460285a506d8dc4d8e3aa81218b7e345be74
Masashi Sato <ms2.sato@gmail.com>
2023-04-08 16:49:25 +0900

    connect api/users

---
+++ frontend/src/views/admin/UsersView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="users">
+        <h1>Users</h1>
+        <table>
+            <tr v-for="user in users" v-bind:key="user.id">
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const users = ref([]);
+onMounted(async () => {
+    const url = "http://localhost:3000/api/users";
+    const res = await axios.get(url);
+    users.value = res.data;
+});
+</script>
