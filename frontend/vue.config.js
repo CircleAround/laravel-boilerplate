d7d391269b1f3ddd95bea8b78a61a81c22b64fd9
Masashi Sato <ms2.sato@gmail.com>
2023-04-13 11:49:00 +0900

    fix js,vue format

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    devServer: {
-        proxy: "http://laravel.test",
-    },
+  devServer: {
+    proxy: 'http://laravel.test'
+  },
 
-    transpileDependencies: true,
-});
+  transpileDependencies: true
+})
--- php-lint.js
+++ php-lint.js
@@ -1,37 +1,37 @@
-const { execSync } = require('child_process');
-const { join } = require('path');
-const { readdirSync, lstatSync } = require('fs');
+const { execSync } = require('child_process')
+const { join } = require('path')
+const { readdirSync, lstatSync } = require('fs')
 
-const DIRECTORY = '.';
-let ERRORS_FOUND = false;
+const DIRECTORY = '.'
+let ERRORS_FOUND = false
 
-const isPhpFile = (file) => file.endsWith('.php') && !file.endsWith('.blade.php');
+const isPhpFile = (file) => file.endsWith('.php') && !file.endsWith('.blade.php')
 
 const checkPhpFile = (file) => {
-  const command = `php -l "${file}"`;
+  const command = `php -l "${file}"`
   try {
-    execSync(command);
+    execSync(command)
   } catch (error) {
-    console.log(error.stdout.toString().trim());
-    ERRORS_FOUND = true;
+    console.log(error.stdout.toString().trim())
+    ERRORS_FOUND = true
   }
-};
+}
 
 const checkDirectory = (dir) => {
   readdirSync(dir).forEach((file) => {
-    const fullPath = join(dir, file);
+    const fullPath = join(dir, file)
     if (lstatSync(fullPath).isDirectory()) {
       if (!fullPath.includes('vendor') && !fullPath.includes('frontend') && !fullPath.includes('storage')) {
-        checkDirectory(fullPath);
+        checkDirectory(fullPath)
       }
     } else if (isPhpFile(file)) {
-      checkPhpFile(fullPath);
+      checkPhpFile(fullPath)
     }
-  });
-};
+  })
+}
 
-checkDirectory(DIRECTORY);
+checkDirectory(DIRECTORY)
 
 if (ERRORS_FOUND) {
-  process.exit(1);
+  process.exit(1)
 }
