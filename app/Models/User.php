1369b410c9fb38ee19881481280645955f6050c3
Masashi Sato <ms2.sato@gmail.com>
2023-05-04 11:50:49 +0900

    fix create user logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
+// ユーザーの権限を示すenum
 enum UserRole: int
 {
-    use \App\Traits\Enum\EnumToArray;
+    use \App\Traits\Enum\EnumToArray; // enumの情報を配列として扱うためのtraits
 
     case Normal = 0;
     case Admin = 1;
@@ -47,6 +49,23 @@ class User extends Authenticatable
     public function isAdmin()
     {
         return $this->role == UserRole::Admin;
+    }
+
+    // 管理者から新規ユーザーを作成する特殊なメソッド。
+    // 作成されたユーザーはemailの認証が済んだ状態になる。
+    public static function createAsVerified($attributes) {
+        $user = new User();
+
+        //　通常許可しない email_verified_at や remember_token を管理者用ロジックから直接書き換えるため一時的にガードを外す
+        User::unguard(); 
+        $user->updateAttributes([
+            ...$attributes, 
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10)
+        ]);
+        User::reguard();
+
+        return $user;
     }
 
     public function updateAttributes($attributes) {
