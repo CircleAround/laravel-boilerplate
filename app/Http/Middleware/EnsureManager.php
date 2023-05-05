889c514321ee70df97bf24f3f9fb2cec3f2b9527
Masashi Sato <ms2.sato@gmail.com>
2023-05-05 15:31:50 +0900

    add ensure manger check

---
+++ app/Http/Middleware/EnsureManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class EnsureManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $team = $request->route()->parameters()['team'];
+        $manager = $team->members()->where(['user_id' => $request->user()->id, 'role' => 1])->first();
+
+        if (!$manager) {
+            return redirect('/')->with('danger', 'アクセスできません');
+        }
+
+        return $next($request);
+    }
+}
