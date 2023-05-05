db3191031ad3fde4fa5af1c6a739b3a1ba732bee
Masashi Sato <ms2.sato@gmail.com>
2023-05-05 15:12:26 +0900

    create team and member in transaction

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
 use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TeamController extends \App\Http\Controllers\Controller
 {
@@ -42,9 +44,19 @@ class TeamController extends \App\Http\Controllers\Controller
             'name' => 'required|max:20',
         ]);
 
-        $team = new Team($validated);
-        $team->owner_id = Auth::user()->id;
-        $team->save();
+        $team = DB::transaction(function () use ($validated) {
+            $team = new Team($validated);
+            $team->owner_id = Auth::user()->id;
+            $team->save();
+
+            $member = new Member();
+            $member->team_id = $team->id;
+            $member->user_id = $team->owner_id;
+            $member->role = 1;
+            $member->save();
+
+            return $team;
+        });
 
         return to_route('manager.teams.show', $team)->with('success', 'チームを作成しました');
     }
