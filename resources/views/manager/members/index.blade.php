a36c14093c7073b28e2401e5417230309bfb267d
Masashi SATO <ms2.sato@gmail.com>
2024-03-09 10:08:50 +0900

    arrangeURL index link

--- resources/views/manager/members/index.blade.php
+++ resources/views/manager/members/index.blade.php
@@ -1,19 +1,19 @@
 <x-layout>
     <x-slot name="title">Teams</x-slot>
-    <h2><a href="{{ route('manager.teams.index') }}">チーム管理</a> / <a
+    <h2><a href="{{ route('teams.index') }}">チーム管理</a> / <a
             href="{{ route('manager.teams.show', $team) }}">{{ $team->name }}(id:{{ $team->id }})</a> / メンバー管理</h2>
 
     <div class="text-end mb-2">
-      <form action="{{ route('manager.teams.members.store', $team) }}" method="POST">
-        @csrf
+        <form action="{{ route('manager.teams.members.store', $team) }}" method="POST">
+            @csrf
 
-        <span>新規メンバー追加: </span>
-        <select name="user_id">
-          @foreach($users as $user) 
-            <option value="{{$user->id}}">{{$user->name}}</option>
-          @endforeach
-        </select>
-        <input type="submit" class="btn btn-primary btn-sm" value="追加">
+            <span>新規メンバー追加: </span>
+            <select name="user_id">
+                @foreach ($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+            <input type="submit" class="btn btn-primary btn-sm" value="追加">
     </div>
 
     <table class="table table-striped align-middle">
@@ -26,15 +26,15 @@
         <tbody>
             @foreach ($team->members as $member)
                 <tr>
-                  <th scope="row">
-                    <!-- 後で適切に実装しましょう -->
-                    @if($member->role === 0)
-                      通常
-                    @else
-                      マネージャー
-                    @endif
-                  </th>
-                  <th scope="row">{{ $member->user->name }}</th>
+                    <th scope="row">
+                        <!-- 後で適切に実装しましょう -->
+                        @if ($member->role === 0)
+                            通常
+                        @else
+                            マネージャー
+                        @endif
+                    </th>
+                    <th scope="row">{{ $member->user->name }}</th>
                 </tr>
             @endforeach
         </tbody>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
     <x-slot name="title">Top</x-slot>
     <a href="{{ route('teams.create') }}">チーム作成</a>
+    <a href="{{ route('teams.index') }}">チーム一覧</a>
 </x-layout>
