c41f1e05af02ae2d0d81ebc498c069751c0d8ee5
Masashi SATO <ms2.sato@gmail.com>
2024-03-18 15:09:57 +0900

    dusk E2E Test

---
+++ tests/Browser/ManagerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+use App\Models\Team;
+
+class TaskTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     */
+    public function testToTeamList(): void
+    {
+        $user = User::factory()->create();
+        $team = Team::factory()->create(['name' => 'TestTeam', 'owner_id' => $user->id]);
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1));
+            $browser->visit('/')->assertSeeLink('チーム一覧');
+            $browser->clickLink('チーム一覧');
+            $browser->assertSeeLink('TestTeam');
+            $browser->screenshot('top');
+        });
+
+        $team->delete();
+        $user->delete();
+    }
+}
